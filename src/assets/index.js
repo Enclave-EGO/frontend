fb3ba59a705cba49f7fd064689d445228ab8e8c9
huynhducthanhtuan <tuan40655@gmail.com>
2023-03-13 10:07:17 +0700

    feat: 🎸 View Lesson

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,12 @@
 // Images
 export { default as SigninBackgroundImage } from "./images/sign-in-background.png";
 export { default as AppLogo } from "./images/logo.png";
+export { default as AvatarImage } from "./images/avatar.png";
 
 // Icons
+export { default as AddIcon } from "./icons/add.png";
+export { default as DropdownIcon } from "./icons/drop-down-arrow.png";
+export { default as PenIcon } from "./icons/pen.png";
+export { default as TrashIcon } from "./icons/trash-can.png";
+export { default as SendIcon } from "./icons/send.png";
+export { default as PlayIcon } from "./icons/play-button.png";
