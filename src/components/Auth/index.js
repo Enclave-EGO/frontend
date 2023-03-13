a04b0d825f6585234e02fef3b5bbfc25504256ff
pv195 <baonguyen1952001@gmail.com>
2023-03-13 11:31:48 +0700

    fix(auth/index): change jwt key

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -12,8 +12,8 @@ export const isAuthenticated = () => {
   if (typeof window == "undefined") {
     return false;
   } else {
-    if (localStorage.getItem("jwt")) {
-      return JSON.parse(localStorage.getItem("jwt"));
+    if (localStorage.getItem("signin_token")) {
+      return JSON.parse(localStorage.getItem("signin_token"));
     } else {
       return false;
     }
