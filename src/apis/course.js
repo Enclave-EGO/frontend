df9e0b4ca842daee802c75ba283c290790c886ae
huynhducthanhtuan <tuan40655@gmail.com>
2023-03-13 14:38:04 +0700

    feat: 🎸 view course

--- src/apis/course.js
+++ src/apis/course.js
@@ -1,12 +1,18 @@
 export const getCourseApi = (courseId) => {
-  return fetch(`/course/${courseId}`, {
-    method: "GET"
+  const token = JSON.parse(localStorage.getItem("signin_token"));
+
+  return fetch(`/courses/${courseId}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token
+    }
   })
     .then((res) => res.json())
     .catch((err) => console.log(err));
 };
 
-export const getCourses = () => {
+export const getCoursesApi = (courseId) => {
   const token = JSON.parse(localStorage.getItem("signin_token"));
 
   return fetch(`/courses`, {
@@ -16,10 +22,6 @@ export const getCourses = () => {
       Authorization: "Bearer " + token
     }
   })
-    .then((res) => {
-      return res.json();
-    })
-    .catch((err) => {
-      return err;
-    });
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 export { default as HomePage } from "./Home";
 export { default as SignupPage } from "./Signup";
 export { default as SigninPage } from "./Signin";
+export { default as CourseDetailPage } from "./CourseDetail";
 export { default as LessonDetailPage } from "./LessonDetail";
 export { default as NotFoundPage } from "./NotFound";
