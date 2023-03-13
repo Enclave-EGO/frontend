c1bbb971b8769d742dfec42017dd38205bb8f245
_ahhtuyet_ <anhtuyetdutk19@gmail.com>
2023-03-13 16:29:54 +0700

    fix: remove comment out

--- src/apis/test.js
+++ src/apis/test.js
@@ -1,6 +1,5 @@
 export const createTest = (newTest) => {
   const token = JSON.parse(localStorage.getItem("signin_token"));
-  console.log(newTest);
 
   return fetch(`/tests`, {
     method: "POST",
