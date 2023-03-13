41af7ebadd5410626a679c6928618b244f62e3bc
huynhducthanhtuan <tuan40655@gmail.com>
2023-03-13 14:54:26 +0700

    fix: 🐛 update get courses api

--- src/apis/course.js
+++ src/apis/course.js
@@ -1,26 +1,14 @@
 export const getCourseApi = (courseId) => {
-  const token = JSON.parse(localStorage.getItem("signin_token"));
-
   return fetch(`/courses/${courseId}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + token
-    }
+    method: "GET"
   })
     .then((res) => res.json())
     .catch((err) => console.log(err));
 };
 
-export const getCoursesApi = (courseId) => {
-  const token = JSON.parse(localStorage.getItem("signin_token"));
-
-  return fetch(`/courses`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + token
-    }
+export const getCoursesApi = () => {
+  return fetch("/courses", {
+    method: "GET"
   })
     .then((res) => res.json())
     .catch((err) => console.log(err));
