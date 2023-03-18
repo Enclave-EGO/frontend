dc370b24100a3307c8cf1e98f3cd4cb9783b3172
huynhducthanhtuan <tuan40655@gmail.com>
2023-03-18 08:27:40 +0700

    chore: temporaly comment

--- src/screens/index.js
+++ src/screens/index.js
@@ -11,10 +11,11 @@ import {
   UpdateLessonPage,
   ManageCoursePage,
   ManageLessonPage,
-  TestTeacherPage
+  TestTeacherPage,
   ManageTestPage,
   UpdateTestPage,
-  NotFoundPage
+  NotFoundPage,
+  TestPage
 } from "../pages";
 
 function Screens() {
@@ -22,17 +23,18 @@ function Screens() {
     <Routes>
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/signin" element={<SigninPage />} />
+      <Route path="/manage/courses" element={<ManageCoursePage />} />
       <Route path="/courses/create" element={<CreateCoursePage />} />
       <Route path="/courses/update/:courseId" element={<UpdateCoursePage />} />
       <Route path="/courses/:courseId" element={<CourseDetailPage />} />
-      <Route path="/lessons/create" element={<CreateLessonPage />} />
-      <Route path="/lessons/update/:lessonId" element={<UpdateLessonPage />} />
-      <Route path="/lessons/:lessonId" element={<LessonDetailPage />} />
-      <Route path="/manage/courses" element={<ManageCoursePage />} />
       <Route path="/manage/lessons" element={<ManageLessonPage />} />
+      <Route path="/lessons/create" element={<CreateLessonPage />} />
+      <Route path="/lessons/:lessonId" element={<LessonDetailPage />} />
+      <Route path="/lessons/update/:lessonId" element={<UpdateLessonPage />} />
       <Route path="/manage/tests" element={<ManageTestPage />} />
+      <Route path="/manage/tests/:testId" element={<TestTeacherPage />} />
       <Route path="/tests/update/:testId" element={<UpdateTestPage />} />
-      <Route path="/tests/:testId" element={<TestTeacherPage />} />
+      <Route path="/tests/:testId" element={<TestPage />} />
       <Route path="/" element={<HomePage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
